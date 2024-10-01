01caaa69d6313cb0098358a9ff94f30b080edada
Jarrod Thompson <47455759+jarrodthompson@users.noreply.github.com>
2024-10-01 19:24:20 +0200

    fixed alles my bru

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/immerse-in-the-virtual-world_D_qUD0K1.mjs
@@ -0,0 +1,9 @@
+async function getMod() {
+						return import('./immerse-in-the-virtual-world_GyKm7dq4.mjs');
+					}
+					const collectedLinks = [];
+					const collectedStyles = [];
+					const collectedScripts = [];
+					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
+
+export { defaultMod as default };
